edeb83e807f219e2fd9dac42ac5ad0ca03843701
ddingcham <qo0op308@gmail.com>
2018-12-06 14:39:45 +0900

    새로운_Reservation이_등록될_때_해당하는_Menu들의_마지막_사용_여부_업데이트 // test Green

--- src/main/java/com/mayak/ddingcham/domain/Menu.java
+++ src/main/java/com/mayak/ddingcham/domain/Menu.java
@@ -101,6 +101,7 @@ public class Menu {
                 .maxCount(maxCount)
                 .build();
         reservations.add(reservation);
+        lastUsed = LAST_USED;
         return reservation;
     }
 
